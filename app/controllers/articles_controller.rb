59a37da13ad13886c10d009b5d03f328237789d6
gitpetr <pkhilenko@yandex.ru>
2016-09-17 10:03:43 +0600

    add categoryes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,13 +3,13 @@ class ArticlesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @articles = Article.all
-    # if params[:category].blank?
-    #   @articles = Article.all.order("created_at DESC")
-    # else
-    #   @category_id = Category.find_by(name: params[:category]).id
-    #   @articles = Article.where(category_id: @category_id).order("created_at DESC")
-    # end
+    #@articles = Article.all
+    if params[:category].blank?
+      @articles = Article.all.order("created_at DESC")
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @articles = Article.where(category_id: @category_id).order("created_at DESC")
+    end
   end
 
   def show
